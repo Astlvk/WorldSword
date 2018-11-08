d0da8dae53ad6fe6235dfeccd6572d222baaa80c
Astlvk <astlvk@vip.qq.com>
2018-11-08 15:15:26 +0800

    create list vue test component

---
+++ src/views/lab/list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>this is vue component</h1>
+    <p>
+      {{ msg }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'message'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
