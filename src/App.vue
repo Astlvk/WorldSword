650fd746acf828a9ce70029d11f4dfb257046ba6
Astlvk <astlvk@vip.qq.com>
2018-12-07 13:34:03 +0800

    首页更改为嵌套路由

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <div id="app">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/list">List</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 <style lang="stylus">
-#app
+/* #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
@@ -30,5 +17,5 @@
     font-weight bold
     color #2c3e50
     &.router-link-exact-active
-      color #42b983
+      color #42b983 */
 </style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import mainContainer from './views/MainContainer.vue'
 
 Vue.use(Router)
 
@@ -8,21 +9,29 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: () => import('./views/lab/list')
+      redirect: 'home',
+      name: 'mainContainer',
+      component: mainContainer,
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: 'about',
+          name: 'about',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+          path: 'list',
+          name: 'list',
+          component: () => import('./views/lab/list')
+        }
+      ]
     }
   ]
 })
