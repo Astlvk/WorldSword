8ba964e8f40b8b315b428c2c6084c50890d64857
Astlvk <astlvk@vip.qq.com>
2018-12-07 17:21:27 +0800

    lab 创建三栏布局

--- src/views/lab/index.vue
+++ src/views/lab/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <router-view></router-view>
+    <el-container>
+      <el-header>
+        <h1>{{ msg }}</h1>
+      </el-header>
+      <el-container>
+        <el-aside style="width: 25%">
+          侧栏
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
